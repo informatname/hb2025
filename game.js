9b32d12bf26c53fa9a2589dd476aa550f478769a
informat <pvv@informat.name>
2025-07-28 12:29:39 +0400

    28 07

--- game.js
+++ game.js
@@ -2,9 +2,11 @@ document.addEventListener("DOMContentLoaded",
   start
 )
 
+let EraserDown=false;
 
 function start() {
   const MenuSize = 123// Временно
+  EraserDown=false;
 
   let canvas = document.getElementById('upScreen');
   if (canvas && canvas.getContext) {
@@ -14,7 +16,7 @@ function start() {
     //canvas.width = window.innerWidth * 0.8 - MenuSize;
     //canvas.height = window.innerHeight * 0.6;
     canvas.width = p.width * 0.9 - MenuSize;
-    canvas.height = p.height * 0.6;
+    canvas.height = p.height * 0.8;
 
     var ctx = canvas.getContext('2d');
 
@@ -36,24 +38,43 @@ function start() {
   }
   
   //canvas.onmousemove="EraserMove(event)"
-  canvas.addEventListener("click", (event) => EraserMove(event));
+  canvas.addEventListener("mousedown", (event) => eraserStart(event));
+  canvas.addEventListener("mouseup", (event) => eraserStop(event));
+  canvas.addEventListener("mousemove", (event) => eraserMove(event));
 }
 
-function Erase(X,Y){
+function erase(X,Y){
 
 let canvas = document.getElementById('upScreen');
   
   if (canvas && canvas.getContext){
   	let ctx = canvas.getContext('2d');
     let p = canvas.getBoundingClientRect();
-     ctx.clearRect(X-p.left-5, Y-p.top-5, 10, 10);
+     //ctx.clearRect(X-p.left-5, Y-p.top-5, 10, 10);
+          let alpha = Math.random()*90;
+          ctx.globalCompositeOperation = 'destination-out';
+          ctx.beginPath();
+          ctx.fillStyle = "rgba(255,255,255,0.9   )";
+          ctx.lineWidth = 50/2;
+          ctx.ellipse(X-p.left-5, Y-p.top-5, 10, 5, alpha, 0 , Math.PI*2, true);
+          ctx.fill();
+          ctx.closePath();
+          ctx.globalCompositeOperation = "source-over";
+
      console.log("Erase: ",X,Y,  p.left, p.top);
   }
   
 }
 
-function EraserMove(E){
- 	 Erase(E.clientX,E.clientY);
-	 //EraserShow(E.clientX,E.clientY);
+function eraserStart(E){
+ 	 erase(E.clientX,E.clientY);
+	 EraserDown=true;
+}
+function eraserMove(E){
+  if( EraserDown )
+ 	   erase(E.clientX,E.clientY);
+}
+function eraserStop(){
+	 EraserDown=false;
 }
 
